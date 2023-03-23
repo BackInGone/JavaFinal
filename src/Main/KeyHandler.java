50fdd5c9404714472938735b98d9d44df3843697
BackInGone <dlsrnjs066@gmail.com>
2023-03-24 02:14:44 +0900

    First

---
+++ src/Main/KeyHandler.java
@@ -0,0 +1,18 @@
+package Main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    public void keyTyped(KeyEvent e){
+
+    }
+    public void keyPressed(KeyEvent e){
+
+    }
+    public void keyReleased(KeyEvent e){
+
+    }
+
+}
